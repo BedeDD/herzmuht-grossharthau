34302d5b742cd3a416f86b9a2babe709a71ce728
Benjamin Deutscher <b.deutscher@felgenoutlet.de>
2020-01-23 18:47:08 +0100

    add styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :pages, param: :seo_url, path: 'seite'
 
   get '/index', to: 'application#home', as: :home
-  get '/hintereingang', to: 'clearance/sessions#new'
   root to: 'application#home'
 end
